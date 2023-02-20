28f047757e0b3bd50565c967bd3a22159f0528dd
tarekahmed199 <tarek.ahmed@nandbox.com>
2023-02-20 03:02:46 +0200

    Initial Commit

---
+++ GameDB/GameDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameDBApp.swift
+//  GameDB
+//
+//  Created by lapshop on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct GameDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
